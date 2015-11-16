c4eb5dc8db3dc9928d449e3320d5770545bdcdf8
jprotivnak <jprotivnak@me.com>
2015-11-16 08:43:54 -0500

    Homework
    Made Die its own file and made changes to main program.

--- src/GameOfTwentyOne.java
+++ src/GameOfTwentyOne.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,7 +16,28 @@ public class GameOfTwentyOne {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner keyboard = new Scanner(System.in);
+		Random rnd = new Random();
+		Die dieRoll1 = new Die(6);
+		Die dieRoll2 = new Die(6);
+		
+		
+		
+		
+		
+		
 	}
 
+	
+	public String response() {
+		String userInput = null;
+		System.out.println( "Please enter 'y' if you'd like to roll the dice or -1 to exit: " );
+		if(userInput.matches("^[Yy]1$") ) {
+			
+		}
+		return count;
+	
+	}
+	
+	
 }
