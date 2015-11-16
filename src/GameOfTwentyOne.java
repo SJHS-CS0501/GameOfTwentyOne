c7219a27768c1b7ec2d8ee40fd40e95ba8808a61
juliannanichols <junichols17@saintjosephhs.com>
2015-11-16 08:46:16 -0500

    Changes

--- src/GameOfTwentyOne.java
+++ src/GameOfTwentyOne.java
@@ -1,18 +1,19 @@
+import javax.swing.JOptionPane;
 /**
- * 
- */
-
-/**
+ * This program will allow a user to play a variation of the popular blackjack card game with the computer.
  * @author woytek
- *
+ * @author Julianna Nichols
  */
 public class GameOfTwentyOne {
 
 	/**
+	 * This is the main method for the GameOfTwentyOne program.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.print( "Let's play a game of blackjack--with dice!" );
+		System.out.print( "Roll the die")
 
 	}
 
