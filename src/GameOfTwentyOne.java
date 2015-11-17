3a1817c89bad865150568ca3fc030a6a60d9c8d8
isschroeder <kittypuppybird@gmail.com>
2015-11-17 09:58:09 -0500

    added if statement
    if statement to continue roll or not

--- src/GameOfTwentyOne.java
+++ src/GameOfTwentyOne.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -18,12 +20,40 @@ public class GameOfTwentyOne {
 	 */
 	public static void main(String[] args) {
 		
-		int dieOne = 0;
-		int dieTwo = 0;
+		Scanner keyboard = new Scanner(System.in);
+		Dice DieOne = new Dice(6);
+		Dice DieTwo = new Dice(6);
+		String cont;
+		int total = 0;
+		int computer = 0;
 		
 		System.out.println( "Let's play a Game of 21!\nIt's like blackjack but with simulated dice!" );
 
+		// put a loop thing in here
 		
+		DieOne.roll();
+		total += DieOne.getValue();
+		
+		DieTwo.roll();
+		total += DieTwo.getValue();
+		
+		System.out.print( "You're total is " + total + ". Would you like to roll the dice again?\n(Enter y for yes and n for no)" );
+		cont = keyboard.nextLine();
+		
+		if( cont.equalsIgnoreCase( "y" )) {
+			DieOne.roll();
+			total += DieOne.getValue();
+			
+			DieTwo.roll();
+			total += DieTwo.getValue();
+			
+			System.out.println( "Your new total is " + total + "." );
+		} else if( cont.equalsIgnoreCase( "n" )) {
+			System.out.print( "You're total is still " + total + "." );
+		}
+		
+		
+
 		
 		
 	}
