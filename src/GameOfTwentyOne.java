adb7a6a2f8c37379b76fa1729da9c365ef9b003b
jprotivnak <jprotivnak@me.com>
2015-11-17 09:58:07 -0500

    Homework
    Added stuff

--- src/GameOfTwentyOne.java
+++ src/GameOfTwentyOne.java
@@ -17,9 +17,10 @@ public class GameOfTwentyOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner keyboard = new Scanner(System.in);
-		Random rnd = new Random();
-		Die dieRoll1 = new Die(6);
-		Die dieRoll2 = new Die(6);
+		
+		System.out.println( "Yo fam, let's play a game... BLACKJACK" );
+		response();
+		
 		
 		
 		
@@ -28,14 +29,35 @@ public class GameOfTwentyOne {
 		
 	}
 
-	
-	public String response() {
+	public static void response() {
 		String userInput = null;
+		int userTotal = 0;
+		Scanner keyboard = new Scanner(System.in);
+		Die dieRoll1 = new Die(6);
+		Die dieRoll2 = new Die(6);
+		String dog = "y";
+		dieRoll1.roll();
+		userTotal += dieRoll1.getValue();
+		dieRoll1.roll();
+		userTotal += dieRoll1.getValue();
+		
 		System.out.println( "Please enter 'y' if you'd like to roll the dice or -1 to exit: " );
-		if(userInput.matches("^[Yy]1$") ) {
+		userInput = keyboard.nextLine();
+		
+		do {
+			System.out.println( "Please enter 'y' if you'd like to roll the dice or -1 to exit: " );
+			userInput = keyboard.nextLine();
 			
-		}
-		return count;
+			if (userInput.matches("^[Yy]$") ) {
+				System.out.println( "Your total is: " + userTotal );
+			} else if (userInput.matches("^-1$") ) {
+				System.out.println( "Your total is: " + userTotal + "\n Thank you for playing!" );
+			} else {
+				System.out.println( "Invalid character entered. Please try again." );
+			}
+		} while( userInput.equalsIgnoreCase(dog))
+		
+		
 	
 	}
 	
