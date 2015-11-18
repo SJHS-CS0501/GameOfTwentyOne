0e844fdba7dc4cad19574b8a596ad6eabc6b01f1
juliannanichols <junichols17@saintjosephhs.com>
2015-11-17 20:15:01 -0500

    Little details
    javadoc, check that logic was correct

--- src/GameOfTwentyOne.java
+++ src/GameOfTwentyOne.java
@@ -15,10 +15,10 @@ public class GameOfTwentyOne {
 	public static void main(String[] args) {
 		
 		Scanner keyboard = new Scanner( System.in );
-		Die roll = new Die(6);
-		int userTotal = 0;
-		int compTotal = 0;
-		int result;
+		Die roll = new Die(6); //creating new Die object
+		int userTotal = 0; //tally of user's total
+		int compTotal = 0; //tally of computer's total
+		int result; //yes or no response if user wants to roll again
 		
 		
 		JOptionPane.showMessageDialog( null, "Let's play a game of blackjack--with dice!\nI just rolled, your turn!" );
@@ -37,26 +37,35 @@ public class GameOfTwentyOne {
 			userTotal += roll.getValue();
 			JOptionPane.showMessageDialog( null, "You rolled an " + roll.getValue() + "!" );
 			
-			result = JOptionPane.showConfirmDialog( null, "Do you want to roll again? Type 'Y' for yes, or 'N' for no." ,
-												"roll?" , JOptionPane.YES_NO_OPTION);
+			result = JOptionPane.showConfirmDialog( null, "Do you want to roll again?" ,
+												"Roll Again?" , JOptionPane.YES_NO_OPTION);
 			
 		}
 		while(result == JOptionPane.YES_OPTION);
 		
-		JOptionPane.showMessageDialog( null, "My total was " + compTotal + "!");
+		JOptionPane.showMessageDialog( null, "My total was " + compTotal + "!" );
 		
+		//when both player's totals are under 21, but computer's total is closer to 21
 		if(compTotal > userTotal && compTotal <= 21) {
 			JOptionPane.showMessageDialog( null, "I win!" );
 		}
 		
-		if(userTotal > 21) {
+		//when user's total is over 21, but computer's total is under 21
+		if(userTotal > 21 && compTotal <= 21) {
 			JOptionPane.showMessageDialog( null, "I win because you went over!" );
 		}
 		
+		//when both player's totals are under 21, but user's total is closer to 21
 		if(userTotal > compTotal && userTotal <= 21) {
 			JOptionPane.showMessageDialog( null, "Yay! You win!" );
 		}
 		
+		//when computer's total is over 21, but user's total is under 21
+		if(compTotal > 21 && userTotal <= 21) {
+			JOptionPane.showMessageDialog( null, "You win because I went over!" );
+		}
+		
+		//when both player's totals are over 21
 		if(userTotal > 21 && compTotal > 21) {
 			JOptionPane.showMessageDialog( null, "Boo :( We both lose!" );
 		}
