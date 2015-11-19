e9bb8ed3ce13c9d6b28f8c59211b52d475e75b38
rysmith18 <rtsmith1101@outlook.com>
2015-11-19 10:29:28 -0500

    Made changes

--- src/GameOfTwentyOne.java
+++ src/GameOfTwentyOne.java
@@ -1,9 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * 
  */
 
 /**
- * @author woytek
+ * @author Ryan Smith
  *
  */
 public class GameOfTwentyOne {
@@ -12,8 +15,138 @@ public class GameOfTwentyOne {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner keyboard = new Scanner(System.in);
+		Die d1 = new Die( 6 );
+		Die d2 = new Die( 6 );
+		char decision;
+		int dieRoll = 0;
+		int computerDieRoll = 0;
+		int counter = 0;
+		
+		System.out.println("Welcome to a game of 21! You will be playing against the computer in a dice game.");
+		
+		do{
+			System.out.println("Would you like to roll the dice? (y to keep playing, anything else to quit)");
+			decision = keyboard.nextLine().toLowerCase().charAt(0);
+			if( decision != 'y') {
+				break;
+			}
+			d1.roll();
+			d2.roll();
+			computerDieRoll += d1.getDieValue();
+			dieRoll += d2.getDieValue();
+			d1.roll();
+			d2.roll();
+			computerDieRoll += d1.getDieValue();
+			dieRoll += d2.getDieValue();
+			System.out.println("Your current score is: " + dieRoll);
+			
+			if( dieRoll == 21 && computerDieRoll != 21 ) {
+				System.out.println("YOU WIN!!!!!!! The computer had: " + computerDieRoll + " You had: " + dieRoll );
+				System.exit(0);
+			}
+			if( dieRoll != 21 && computerDieRoll != 21 ) {
+				System.out.println("Better luck next time! The computer had: " + computerDieRoll + " You had: " + dieRoll);
+				System.exit(0);
+			}
+			if( dieRoll == 21 && computerDieRoll == 21 ) {
+				System.out.println("Good game! It was a draw.  You both had: " + dieRoll);
+				System.exit(0);
+			}
+			if( dieRoll < 21 && computerDieRoll > 21 ) {
+				System.out.println("YOU WIN!!!!!!! The computer had: " + computerDieRoll + " You had: " + dieRoll );
+				System.exit(0);
+			}
+			if( dieRoll > 21 && computerDieRoll < 21 ) {
+				System.out.println("Better luck next time! The computer had: " + computerDieRoll + " You had: " + dieRoll);
+				System.exit(0);
+			}
+			if( dieRoll < computerDieRoll && computerDieRoll < 21 ) {
+				System.out.println("Better luck next time! The computer had: " + computerDieRoll + " You had: " + dieRoll);
+				System.exit(0);
+			}
+			if( computerDieRoll < dieRoll && dieRoll < 21 ) {
+				System.out.println("YOU WIN!!!!!!! The computer had: " + computerDieRoll + " You had: " + dieRoll );
+				System.exit(0);
+			}
+			if( dieRoll > 21 && computerDieRoll > 21 && dieRoll > computerDieRoll ) {
+				System.out.println("Better luck next time! The computer had: " + computerDieRoll + " You had: " + dieRoll);
+				System.exit(0);
+			}
+			if( dieRoll > 21 && computerDieRoll > 21 && computerDieRoll > dieRoll ) {
+				System.out.println("YOU WIN!!!!!!! The computer had: " + computerDieRoll + " You had: " + dieRoll);
+				System.exit(0);
+			}
+			if( dieRoll > 21 && computerDieRoll > 21 && computerDieRoll == dieRoll ) {
+				System.out.println("Good game! It was a draw.  You both had: " + dieRoll);
+				System.exit(0);
+			}
+		} while( dieRoll < 21 && computerDieRoll < 21);
+		
+		if( dieRoll == 21 && computerDieRoll != 21 ) {
+			System.out.println("YOU WIN!!!!!!! The computer had: " + computerDieRoll + " You had: " + dieRoll );
+			System.exit(0);
+		}
+		if( dieRoll != 21 && computerDieRoll != 21 ) {
+			System.out.println("Better luck next time! The computer had: " + computerDieRoll + " You had: " + dieRoll);
+			System.exit(0);
+		}
+		if( dieRoll == 21 && computerDieRoll == 21 ) {
+			System.out.println("Good game! It was a draw.  You both had: " + dieRoll);
+			System.exit(0);
+		}
+		if( dieRoll < 21 && computerDieRoll > 21 ) {
+			System.out.println("YOU WIN!!!!!!! The computer had: " + computerDieRoll + " You had: " + dieRoll );
+			System.exit(0);
+		}
+		
+		if( dieRoll > 21 && computerDieRoll < 21 ) {
+			System.out.println("Better luck next time! The computer had: " + computerDieRoll + " You had: " + dieRoll);
+			System.exit(0);
+		}
+		if( dieRoll > 21 && computerDieRoll > 21 && dieRoll > computerDieRoll ) {
+			System.out.println("Better luck next time! The computer had: " + computerDieRoll + " You had: " + dieRoll);
+			System.exit(0);
+		}
+		if( dieRoll > 21 && computerDieRoll > 21 && computerDieRoll > dieRoll ) {
+			System.out.println("YOU WIN!!!!!!! The computer had: " + computerDieRoll + " You had: " + dieRoll);
+			System.exit(0);
+		}
+		if( dieRoll < computerDieRoll && computerDieRoll < 21 ) {
+			System.out.println("Better luck next time! The computer had: " + computerDieRoll + " You had: " + dieRoll);
+			System.exit(0);
+		}
+		if( computerDieRoll < dieRoll && dieRoll < 21 ) {
+			System.out.println("YOU WIN!!!!!!! The computer had: " + computerDieRoll + " You had: " + dieRoll );
+			System.exit(0);
+		}
 
+	}
+	
+	public static class Die {
+		private int dieFaces;
+		private int dieValue;
+		
+		public Die(int numDieFaces ) {
+			dieFaces = numDieFaces;
+			roll();
+		}
+		
+		public void roll() {
+			Random random = new Random();
+			
+			dieValue = random.nextInt(dieFaces) + 1;
+		}
+		
+		public int getDieFaces() {
+			return dieFaces;
+		}
+		
+		public int getDieValue() {
+			return dieValue;
+		}
 	}
 
 }
+
