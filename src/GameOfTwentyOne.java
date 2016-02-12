a464514a44f320d6a4dbbb609692b8449bdbabc7
juliannanichols <junichols17@saintjosephhs.com>
2016-02-12 10:30:59 -0500

    fixed but not (ActionListener)

--- src/GameOfTwentyOne.java
+++ src/GameOfTwentyOne.java
@@ -13,7 +13,6 @@ public class GameOfTwentyOne {
 	
 	static JLabel label;
     static JButton button;
-    static JButton buttonN;
     static JTextField textField;
     static GridBagLayout layout;
 
@@ -23,12 +22,11 @@ public class GameOfTwentyOne {
 	 */
 	public static void main(String[] args) {
 		
-		Scanner keyboard = new Scanner( System.in );
-		Die roll = new Die(6); //creating new Die object
+		
 		int userTotal = 0; //tally of user's total
 		int compTotal = 0; //tally of computer's total
-		int result; //yes or no response if user wants to roll again
-		ActionListener listener = new ActionListener();
+		
+		ActionListener listener = new RollAgainListener();
 		layout = new GridBagLayout();
 		
 		JFrame frame = new JFrame("Game of Twenty-One");
@@ -59,42 +57,27 @@ public class GameOfTwentyOne {
         layout.setConstraints( button, c);
         frame.add(button);
         
+        button.addActionListener(listener);
+        
         do {
-			roll.roll(); //first die
-			compTotal += roll.getValue();
-			roll.roll(); //second die
-			compTotal += roll.getValue();
-			
-			roll.roll(); //first die
-			userTotal += roll.getValue();
-			label = new JLabel("You rolled an " + roll.getValue() + "!");
-			label.setForeground(Color.BLACK);
-			
-			roll.roll(); //second die
-			userTotal += roll.getValue();
-			label = new JLabel("You rolled an " + roll.getValue() + "!");
-			label.setForeground(Color.BLACK);
-			
-			label = new JLabel("Do you want to roll again?");
-			label.setForeground(Color.BLACK);
-			
-			//making yes button
-	        button = new JButton("Yes");
-	        //setting the font and font size of yes button
-	        button.setFont(new Font("TimesRoman", Font.PLAIN, 12));
-	        
-	        //making no button
-	        buttonN = new JButton("No");
-	        //setting the font and font size of no button
-	        buttonN.setFont(new Font("TimesRoman", Font.PLAIN, 12));
-	        
-	        button.addActionListener(new RollAgainListener());
-	        buttonN.addActionListener(new RollAgainListener());
-	        
+        label = new JLabel("Do you want to roll again?");
+		label.setForeground(Color.BLACK);
+		layout.setConstraints( label, c);
+		
+        //making no button
+        button = new JButton("No");
+        //setting the font and font size of no button
+        button.setFont(new Font("TimesRoman", Font.PLAIN, 12));
+		layout.setConstraints( label, c);
+        
+        button.addActionListener(listener);
         } while(button.equals(true));
-
+        
         label = new JLabel("My total was " + compTotal + "!");
 		label.setForeground(Color.BLACK);
+		layout.setConstraints( label, c);
+
+       
 		
         /*
 		JOptionPane.showMessageDialog( null, "Let's play a game of blackjack--with dice!\nI just rolled, your turn!" );
@@ -127,30 +110,35 @@ public class GameOfTwentyOne {
 		if(compTotal > userTotal && compTotal <= 21) {
 			label = new JLabel("I win!");
 			label.setForeground(Color.BLACK);
+			layout.setConstraints( label, c);
 		}
 		
 		//when user's total is over 21, but computer's total is under 21
 		if(userTotal > 21 && compTotal <= 21) {			
 			label = new JLabel("I win because you went over!");
 			label.setForeground(Color.BLACK);
+			layout.setConstraints( label, c);
 		}
 		
 		//when both player's totals are under 21, but user's total is closer to 21
 		if(userTotal > compTotal && userTotal <= 21) {
 			label = new JLabel("Yay! You win!");
 			label.setForeground(Color.BLACK);
+			layout.setConstraints( label, c);
 		}
 		
 		//when computer's total is over 21, but user's total is under 21
 		if(compTotal > 21 && userTotal <= 21) {
 			label = new JLabel("You win because I went over!");
 			label.setForeground(Color.BLACK);
+			layout.setConstraints( label, c);
 		}
 		
 		//when both player's totals are over 21
 		if(userTotal > 21 && compTotal > 21) {
 			label = new JLabel("Boo :( We both lose!");
 			label.setForeground(Color.BLACK);
+			layout.setConstraints( label, c);
 		}
 		
 		frame.pack();
@@ -158,16 +146,43 @@ public class GameOfTwentyOne {
 
 	}
 	
-	private class RollAgainListener implements ActionListener {
+	static class RollAgainListener implements ActionListener {
 		
 		/**
 		 * The actionPerformed method executes when the user clicks on the yes or no button in
 		 * response to the question if they want to roll again
 		 * @param e the event object
+		 * @return 
 		 */
 		public void actionPerformed(ActionEvent e) {
+			Die roll = new Die(6); //creating new Die object
+			int userTotal = 0; //tally of user's total
+			int compTotal = 0; //tally of computer's total
 			
-		}
+			GridBagConstraints c = new GridBagConstraints();
+			c.fill = GridBagConstraints.BOTH;
+			c.weightx = 0.0;
+		    c.gridwidth = GridBagConstraints.RELATIVE;
+			
+			JButton eventSource = (JButton)e.getSource();
+            if( eventSource.equals( "Roll" ) ) {
+        			roll.roll(); //first die
+        			compTotal += roll.getValue();
+        			roll.roll(); //second die
+        			compTotal += roll.getValue();
+        			
+        			roll.roll(); //first die
+        			userTotal += roll.getValue();
+        			label = new JLabel("You rolled an " + roll.getValue() + "!");
+        			label.setForeground(Color.BLACK);
+        			layout.setConstraints( label, c);
+        			
+        			roll.roll(); //second die
+        			userTotal += roll.getValue();
+        			label = new JLabel("You rolled an " + roll.getValue() + "!");
+        			label.setForeground(Color.BLACK);	
+        			layout.setConstraints( label, c);
+            }
+        }
 	}
-	
 }
