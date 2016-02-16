03eb23193caa30b42fb503f0e2ee0f57d0ae7194
juliannanichols <junichols17@saintjosephhs.com>
2016-02-15 19:21:23 -0500

    YES
    Fixed problems...fully functioning....next to it wont terminate until
    you hit the close button..minor problem..but otherwise..conforms to
    assignment requirements

--- src/GameOfTwentyOne.java
+++ src/GameOfTwentyOne.java
@@ -4,16 +4,21 @@ import javax.swing.*;
 
 /**
  * This program will allow a user to play a variation of the popular blackjack card game with the computer.
- * @author woytek
+ * @author Woytek
  * @author Julianna Nichols
  */
 public class GameOfTwentyOne {
 	
 	static JLabel label;
-    static JButton button;
-    static JTextField textField;
-    static GridBagLayout layout;
-    static JFrame frame = new JFrame("Game of Twenty-One"); //new JFrame; 
+	static JLabel die;
+    static JButton roll;
+    static JButton no;
+    static GridBagLayout layout = new GridBagLayout();
+    static JFrame frame = new JFrame("Game of Twenty-One"); //new JFrame
+    static int userTotal = 0; //user's total
+	static int compTotal = 0; //computer's total
+	static Die rollDie = new Die(6); //creating new Die object
+	static GridBagConstraints c = new GridBagConstraints();
 
 	/**
 	 * This is the main method for the GameOfTwentyOne program.
@@ -21,217 +26,197 @@ public class GameOfTwentyOne {
 	 */
 	public static void main(String[] args) {
 		
-		
-		int userTotal = 0; //tally of user's total
-		int compTotal = 0; //tally of computer's total
-		
-		ActionListener listener = new RollAgainListener();
-		layout = new GridBagLayout();
+		ActionListener listener = new Listener();
 	
-		//JFrame frame = new JFrame("Game of Twenty-One"); //new JFrame
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //default close operation
 		
-		frame.setLayout( layout );
-		
-		GridBagConstraints c = new GridBagConstraints();
+		frame.setLayout( layout ); //giving the frame the designated layout
 	    
-	    frame.setVisible(true);
+	    frame.setVisible(true); //making the frame visible to the screen
 		
-	    textField = new JTextField("Let's play a game of blackjack--with dice! I just rolled, your turn!");
-	    textField.setForeground(Color.BLUE);
-	    textField.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+	    label = new JLabel("Let's play a game of blackjack--with dice! I just rolled, your turn!");
+	    label.setForeground(Color.BLACK);
+	    label.setFont(new Font("TimesRoman", Font.PLAIN, 24));
         c.fill = GridBagConstraints.BOTH; //what to do when the component's display area is larger than the component's requested size
         c.weightx = 0.2; //resizing behavior (column)
 		c.weighty = 0.2; //resizing behavior (row)
 		c.gridx = 0; //second column
 		c.gridy = 0; //first row
-		c.gridwidth = 3;
-        frame.add(textField, c);
+		c.gridwidth = 3; //take up three columns
+        frame.add(label, c);
 
-        //c.gridwidth = GridBagConstraints.REMAINDER;
+        /*
+         * This label holds space.
+         * 
+         * Before this was added, my first roll statement (You rolled an _!) would write over itself..this is to fix that.
+         * In the actionReader, I have "label.setVisible(false);" this is making the second roll statement (You rolled an _ !)
+         * disappear before it prints the next one.
+         * 
+         * So this label is holding that space for the first roll statement..so I could write "die.setVisible(false)" so before
+         * prints the next roll statement the previous one disappears
+         * 
+         * I understand this is confusing, and bad programming..
+         * I can go back and change it later and make it more efficient
+         */
+        die = new JLabel("");
+        frame.add(die, c);
         
-        //making roll button
-        button = new JButton("Roll");
-        //setting the font and font size of roll button
-        button.setFont(new Font("TimesRoman", Font.PLAIN, 12));
+        roll = new JButton("Roll");
+        roll.setFont(new Font("TimesRoman", Font.PLAIN, 12));
         c.fill = GridBagConstraints.BOTH; //what to do when the component's display area is larger than the component's requested size
         c.weightx = 0.2; //resizing behavior (column)
 		c.weighty = 0.2; //resizing behavior (row)
-		c.gridx = 2; //roll button in 
-		c.gridy = 1;
-		c.gridwidth = 1;
-        frame.add(button, c);
-        
-        button.addActionListener(listener);
-        
-        
-		for( int i = 1; i < 2; i-- ) {
-			
-			// making no button
-			button = new JButton("No");
-			// setting the font and font size of no button
-			button.setFont(new Font("TimesRoman", Font.PLAIN, 12));
-			// what to do when the component's display area is larger than the component's requested size
-			c.fill = GridBagConstraints.BOTH;
-			c.weightx = 0.2; // resizing behavior (column)
-			c.weighty = 0.2; // resizing behavior (row)
-			c.gridx = 0;
-			c.gridy = 1;
-			c.gridwidth = 1;
-			frame.add(button, c);
-			button.addActionListener(listener);
-			
-			if(button.getText().equals("No")) {
-				i = 2;
-			}
-			
-		}
+		c.gridx = 2; //third column
+		c.gridy = 1; //second row
+		c.gridwidth = 1; //take up one column
+		frame.add(roll, c);
+		roll.addActionListener(listener); //actionListener listening for roll button
 
-        /*
-        label = new JLabel("My total was " + compTotal + "!");
-		label.setForeground(Color.BLACK);
-		layout.setConstraints( label, c);
-        
-		//when both player's totals are under 21, but computer's total is closer to 21
-		if(compTotal > userTotal && compTotal <= 21) {
-			label = new JLabel("I win!");
-			label.setForeground(Color.BLACK);
-			layout.setConstraints( label, c);
-		}
-		
-		//when user's total is over 21, but computer's total is under 21
-		if(userTotal > 21 && compTotal <= 21) {			
-			label = new JLabel("I win because you went over!");
-			label.setForeground(Color.BLACK);
-			layout.setConstraints( label, c);
-		}
-		
-		//when both player's totals are under 21, but user's total is closer to 21
-		if(userTotal > compTotal && userTotal <= 21) {
-			label = new JLabel("Yay! You win!");
-			label.setForeground(Color.BLACK);
-			layout.setConstraints( label, c);
-		}
-		
-		//when computer's total is over 21, but user's total is under 21
-		if(compTotal > 21 && userTotal <= 21) {
-			label = new JLabel("You win because I went over!");
-			label.setForeground(Color.BLACK);
-			layout.setConstraints( label, c);
-		}
-		
-		//when both player's totals are over 21
-		if(userTotal > 21 && compTotal > 21) {
-			label = new JLabel("Boo :( We both lose!");
-			label.setForeground(Color.BLACK);
-			layout.setConstraints( label, c);
-		}
-		
-		*/
+		no = new JButton("No");
+		no.setFont(new Font("TimesRoman", Font.PLAIN, 12));
+		c.fill = GridBagConstraints.BOTH; //what to do when the component's display area is larger than the component's requested size
+		c.weightx = 0.2; //resizing behavior (column)
+		c.weighty = 0.2; //resizing behavior (row)
+		c.gridx = 1; //second column
+		c.gridy = 1; //second row
+		c.gridwidth = 1; //take up one column
+		frame.add(no, c);
+		no.addActionListener(listener); //actionListener listening for no button
 		
 		frame.pack();
         frame.setSize( frame.getPreferredSize() );
 
 	}
 	
-	static class RollAgainListener implements ActionListener {
+	/**
+	 * ActionListener will react to each button click
+	 * @author Julianna Nichols
+	 */
+	static class Listener implements ActionListener {
 		
 		/**
-		 * The actionPerformed method executes when the user clicks on the yes or no button in
-		 * response to the question if they want to roll again
-		 * @param e the event object
+		 * If "Roll" button is clicked, rolls die
+		 * If "No" button is clicked, compares computer's total to user's total
 		 * @return 
 		 */
 		public void actionPerformed(ActionEvent e) {
-			Die roll = new Die(6); //creating new Die object
-			int userTotal = 0; //tally of user's total
-			int compTotal = 0; //tally of computer's total
-			GridBagConstraints c = new GridBagConstraints();
-			ActionListener listener = new RollAgainListener();
-			
 			JButton eventSource = (JButton)e.getSource();
-			if(eventSource.getText().equals("Roll")) {
-        			roll.roll(); //first die
-        			compTotal += roll.getValue();
-        			roll.roll(); //second die
-        			compTotal += roll.getValue();
-        			
-        			roll.roll(); //first die
-        			userTotal += roll.getValue();
-        			//what to do when the component's display area is larger than the component's requested size
-        			c.fill = GridBagConstraints.BOTH;
-        			c.gridx = 2;
-        			c.gridy = 0;
-        			c.weightx = 0.2; //resizing behavior (column)
-        			c.weighty = 0.2; //resizing behavior (row)
-        			c.gridwidth = 1;
-        			textField.setText("You rolled an " + roll.getValue() + "!");
-        			textField.setForeground(Color.BLACK);
-        			frame.add(textField, c);
-        			
-        			roll.roll(); //second die
-        			userTotal += roll.getValue();
-        			//what to do when the component's display area is larger than the component's requested size
-        			c.fill = GridBagConstraints.BOTH;
-        			c.gridx = 2;
-        			c.gridy = 0;
-        			c.weightx = 0.2; //resizing behavior (column)
-        			c.weighty = 0.2; //resizing behavior (row)
-        			c.gridwidth = 1;
-        			textField.setText("You rolled an " + roll.getValue() + "!");
-        			textField.setForeground(Color.BLACK);
-        			frame.add(textField, c);
-        			
-        			textField = new JTextField("Do you want to roll again?");
-        			textField.setForeground(Color.BLACK);
-        			textField.setFont(new Font("TimesRoman", Font.PLAIN, 24));
-        			// what to do when the component's display area is larger than the component's requested size
-        			c.fill = GridBagConstraints.BOTH;
-        			c.weightx = 0.2; // resizing behavior (column)
-        			c.weighty = 0.2; // resizing behavior (row)
-        			c.gridx = 1;
-        			c.gridy = 1; // first row
-        			frame.add(textField, c);
+			
+			if (eventSource.getText().equals("Roll")) {
+				label.setVisible(false);
+				die.setVisible(false);
+				
+				rollDie.roll(); // first die for computer
+				compTotal += rollDie.getValue();
+				rollDie.roll(); // second die for computer
+				compTotal += rollDie.getValue();
+
+				rollDie.roll(); // first die for user
+				userTotal += rollDie.getValue();
+				c.fill = GridBagConstraints.BOTH;
+				c.weightx = 0.2; // resizing behavior (column)
+				c.weighty = 0.2; // resizing behavior (row)
+				c.gridx = 1; // second column
+				c.gridy = 0; // first row
+				c.gridwidth = 1; // take up one column
+				die = new JLabel("You rolled an " + rollDie.getValue() + "!");
+				die.setForeground(Color.BLACK);
+				die.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+				frame.add(die, c);
+
+				rollDie.roll(); // second die for user
+				userTotal += rollDie.getValue();
+				c.fill = GridBagConstraints.BOTH;
+				c.weightx = 0.2; // resizing behavior (column)
+				c.weighty = 0.2; // resizing behavior (row)
+				c.gridx = 2; // third column
+				c.gridy = 0; // first row
+				c.gridwidth = 1; // take up one column
+				label = new JLabel("You rolled an " + rollDie.getValue() + "! Roll again?");
+				label.setForeground(Color.BLACK);
+				label.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+				frame.add(label, c);
 			}
 			
 			if(eventSource.getText().equals("No")) {
-				textField = new JTextField("My total was " + compTotal + "!");
-				textField.setForeground(Color.BLACK);
-				layout.setConstraints( textField, c);
+				
+				label.setVisible(false);
+				die.setVisible(false);
+				roll.setVisible(false);
+				no.setVisible(false);
+				
+				label = new JLabel("My total was " + compTotal + "!");
+				label.setForeground(Color.BLACK);
+				label.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+				c.weightx = 0.2; // resizing behavior (column)
+    			c.weighty = 0.2; // resizing behavior (row)
+    			c.gridx = 0; //first column
+    			c.gridy = 0; // first row
+				layout.setConstraints( label, c);
+				frame.add(label, c);
 		        
-				//when both player's totals are under 21, but computer's total is closer to 21
+				//both player's totals < 21, but computer's total is closer to 21
 				if(compTotal > userTotal && compTotal <= 21) {
-					textField = new JTextField("I win!");
-					textField.setForeground(Color.BLACK);
-					layout.setConstraints( textField, c);
+					label = new JLabel("I win!");
+					label.setForeground(Color.BLACK);
+					label.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+					c.weightx = 0.2; // resizing behavior (column)
+        			c.weighty = 0.2; // resizing behavior (row)
+        			c.gridx = 2; //first column
+        			c.gridy = 0; // first row
+					layout.setConstraints( label, c);
+					frame.add(label, c);
 				}
 				
-				//when user's total is over 21, but computer's total is under 21
+				//user's total > 21, but computer's total < 21
 				if(userTotal > 21 && compTotal <= 21) {			
-					textField = new JTextField("I win because you went over!");
-					textField.setForeground(Color.BLACK);
-					layout.setConstraints( textField, c);
+					label = new JLabel("I win because you went over!");
+					label.setForeground(Color.BLACK);
+					label.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+					c.weightx = 0.2; // resizing behavior (column)
+        			c.weighty = 0.2; // resizing behavior (row)
+        			c.gridx = 2; //first column
+        			c.gridy = 0; // first row
+					layout.setConstraints( label, c);
+					frame.add(label, c);
 				}
 				
-				//when both player's totals are under 21, but user's total is closer to 21
+				//both player's totals < 21, but user's total is closer to 21
 				if(userTotal > compTotal && userTotal <= 21) {
-					textField = new JTextField("Yay! You win!");
-					textField.setForeground(Color.BLACK);
-					layout.setConstraints( textField, c);
+					label = new JLabel("Yay! You win!");
+					label.setForeground(Color.BLACK);
+					label.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+					c.weightx = 0.2; // resizing behavior (column)
+        			c.weighty = 0.2; // resizing behavior (row)
+        			c.gridx = 2; //first column
+        			c.gridy = 0; // first row
+					layout.setConstraints( label, c);
+					frame.add(label, c);
 				}
-				
-				//when computer's total is over 21, but user's total is under 21
+				//computer's total > 21, but user's total < 21
 				if(compTotal > 21 && userTotal <= 21) {
-					textField = new JTextField("You win because I went over!");
-					textField.setForeground(Color.BLACK);
-					layout.setConstraints( textField, c);
+					label = new JLabel("You win because I went over!");
+					label.setForeground(Color.BLACK);
+					label.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+					c.weightx = 0.2; // resizing behavior (column)
+        			c.weighty = 0.2; // resizing behavior (row)
+        			c.gridx = 2; //first column
+        			c.gridy = 0; // first row
+					layout.setConstraints( label, c);
+					frame.add(label, c);
 				}
 				
-				//when both player's totals are over 21
+				//when both player's totals > 21
 				if(userTotal > 21 && compTotal > 21) {
-					textField = new JTextField("Boo :( We both lose!");
-					textField.setForeground(Color.BLACK);
-					layout.setConstraints( textField, c);
+					label = new JLabel("Boo :( We both lose!");
+					label.setForeground(Color.BLACK);
+					label.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+					c.weightx = 0.2; // resizing behavior (column)
+        			c.weighty = 0.2; // resizing behavior (row)
+        			c.gridx = 2; //first column
+        			c.gridy = 0; // first row
+					layout.setConstraints( label, c);
+					frame.add(label, c);
 				}
 			}
         }
